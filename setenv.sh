466209afba96a901cfb92cd6c22111e7367d2da4
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-07-07 10:41:22 +0200

    Update env vars

--- setenv.sh
+++ setenv.sh
@@ -3,20 +3,20 @@
 # Launch script for Abc-Map
 # You can modify environment vars to modify site behavior
 
-export SAFRANLICES_DB_NAME=abcmapfr
+export SAFRANLICES_FR_DB_NAME=safranlices
 # Specify JDBC url without database name
-export SAFRANLICES_DB_URL=jdbc:mysql://localhost:3306/
-export SAFRANLICES_DB_USERNAME=safranlices
-export SAFRANLICES_DB_PASSWORD=safranlices
+export SAFRANLICES_FR_DB_URL=jdbc:mysql://localhost:3306/
+export SAFRANLICES_FR_DB_USERNAME=safranlices
+export SAFRANLICES_FR_DB_PASSWORD=safranlices
 
 
-export SAFRANLICES_DB_BACKUP_DIRECTORY=./db-backup/
-export SAFRANLICES_DB_BACKUP_NUMBER=40
+export SAFRANLICES_FR_DB_BACKUP_DIRECTORY=./db-backup/
+export SAFRANLICES_FR_DB_BACKUP_NUMBER=40
 
-export SAFRANLICES_ADMIN_LOGIN=admin
-export SAFRANLICES_ADMIN_PASSWORD=admin
+export SAFRANLICES_FR_ADMIN_LOGIN=admin
+export SAFRANLICES_FR_ADMIN_PASSWORD=admin
 
-export SAFRANLICES_ACTIVE_PROFILE=dev
+export SAFRANLICES_FR_ACTIVE_PROFILE=dev
 
 
 
