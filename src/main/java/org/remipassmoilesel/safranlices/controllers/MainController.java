a04b4e80bc0b8384eb1422de72d9faccb90db104
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-07-07 10:42:09 +0200

    Add main mappings, improve navbar

--- src/main/java/org/remipassmoilesel/safranlices/controllers/MainController.java
+++ src/main/java/org/remipassmoilesel/safranlices/controllers/MainController.java
@@ -5,6 +5,8 @@ import org.remipassmoilesel.safranlices.Templates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,27 +18,32 @@ public class MainController {
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping(Mappings.TEMPLATE)
-    public String showTemplate() {
+    public String showTemplate(Model model) {
+        Mappings.includeMappings(model);
         return Templates.MAIN_TEMPLATE;
     }
 
     @RequestMapping(Mappings.ROOT)
-    public String showWelcome() {
+    public String showWelcome(Model model) {
+        Mappings.includeMappings(model);
         return Templates.WELCOME;
     }
 
     @RequestMapping(Mappings.PRODUCTS)
-    public String showProducts() {
+    public String showProducts(Model model) {
+        Mappings.includeMappings(model);
         return Templates.PRODUCTS;
     }
 
     @RequestMapping(Mappings.EXPLOITATION)
-    public String showExploitation() {
+    public String showExploitation(Model model) {
+        Mappings.includeMappings(model);
         return Templates.EXPLOITATION;
     }
 
     @RequestMapping(Mappings.ORDER)
-    public String showOrder() {
+    public String showOrder(Model model) {
+        Mappings.includeMappings(model);
         return Templates.ORDER;
     }
 
