cdbe995061581ae895836be326a6ac3a7c9bb269
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-07-09 10:48:21 +0200

    Improve order view

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,17 +1,25 @@
 var App = {
 
-    init: function(){
-        $(".add-to-basket").click(function(){
+    init: function () {
+        $(".add-to-basket").click(function () {
 
             var id = $(this).data('id');
             var max = $(this).data('max');
             var qtty = $("#productQuantity" + id).val();
             var errorArea = $("#errorArea" + id);
 
+            errorArea.css("opacity", "0");
+
             // check if not ordering too much
-            if(qtty > max){
+            if (qtty > max) {
                 errorArea.html("Vous ne pouvez pas commander plus de " + max + " articles.");
-                errorArea.css("display", "block");
+                errorArea.animate({"opacity": 1}, 700);
+                return;
+            }
+
+            if (qtty < 1) {
+                errorArea.html("Vous ne pouvez pas commander moins de 1 article.");
+                errorArea.animate({"opacity": 1}, 700);
                 return;
             }
 
@@ -22,6 +30,6 @@ var App = {
 
 };
 
-$(function(){
-   App.init();
+$(function () {
+    App.init();
 });
