eb9d603d3892a1e15e4b47443b95d24dce01b64d
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-07-09 22:29:34 +0200

    Improve tests

---
+++ src/test/java/org/remipassmoilesel/safranlices/BasketTest.java
@@ -0,0 +1,118 @@
+package org.remipassmoilesel.safranlices;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.remipassmoilesel.safranlices.controllers.MainController;
+import org.remipassmoilesel.safranlices.entities.Product;
+import org.remipassmoilesel.safranlices.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by remipassmoilesel on 09/07/17.
+ */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles(SafranLicesApplication.DEV_PROFILE)
+public class BasketTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController mainController;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Before
+    public void setup() throws IOException {
+        mockMvc = MockMvcBuilders.standaloneSetup(mainController).build();
+    }
+
+    @Test
+    public void testBasket() throws Exception {
+
+        // show basket
+        MvcResult response = mockMvc.perform(get(Mappings.BASKET))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME)).andReturn();
+
+        MockHttpSession session = (MockHttpSession) response.getRequest().getSession();
+
+        // add product to basket
+        Integer qtty = 6;
+        List<Product> products = productRepository.findAll();
+
+        mockMvc.perform(get(Mappings.BASKET)
+                .session(session)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("addToCart", "true")
+                .param("qtty", String.valueOf(qtty))
+                .param("id", String.valueOf(products.get(0).getId())));
+
+        mockMvc.perform(get(Mappings.BASKET)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .session(session)
+                .param("addToCart", "true")
+                .param("qtty", String.valueOf(qtty))
+                .param("id", String.valueOf(products.get(1).getId())))
+                .andExpect(status().is3xxRedirection());
+
+        response = mockMvc.perform(get(Mappings.BASKET)
+                .session(session)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("addToCart", "true")
+                .param("qtty", String.valueOf(qtty))
+                .param("id", String.valueOf(products.get(2).getId())))
+                .andExpect(status().is3xxRedirection()).andReturn();
+
+        HashMap<Long, Integer> basket = (HashMap<Long, Integer>) response.getRequest().getSession().getAttribute(MainController.BASKET_SATTR);
+        Integer q = basket.values().iterator().next();
+        Long pid = basket.keySet().iterator().next();
+        assertTrue(basket.size() == 3);
+        assertTrue(pid.equals(products.get(0).getId()));
+        assertTrue(q.equals(qtty));
+
+        // delete a product
+        response = mockMvc.perform(get(Mappings.BASKET)
+                .session(session)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("delete", "true")
+                .param("id", String.valueOf(products.get(0).getId())))
+                .andExpect(status().is3xxRedirection()).andReturn();
+
+        basket = (HashMap<Long, Integer>) response.getRequest().getSession().getAttribute(MainController.BASKET_SATTR);
+        assertTrue(basket.size() == 2);
+        assertTrue(basket.keySet().iterator().next() == products.get(1).getId());
+
+        // empty basket
+        mockMvc.perform(get(Mappings.BASKET)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("reset", "true"))
+                .andExpect(status().is3xxRedirection());
+
+        basket = (HashMap<Long, Integer>) response.getRequest().getSession().getAttribute(MainController.BASKET_SATTR);
+        assertTrue(basket.size() == 0);
+
+
+    }
+
+}
--- src/test/java/org/remipassmoilesel/safranlices/MainControllerTest.java
+++ src/test/java/org/remipassmoilesel/safranlices/MainControllerTest.java
@@ -4,16 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.remipassmoilesel.safranlices.controllers.MainController;
+import org.remipassmoilesel.safranlices.entities.Product;
+import org.remipassmoilesel.safranlices.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +39,9 @@ public class MainControllerTest {
     @Autowired
     private MainController mainController;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Before
     public void setup() throws IOException {
         mockMvc = MockMvcBuilders.standaloneSetup(mainController).build();
@@ -38,6 +49,7 @@ public class MainControllerTest {
 
     @Test
     public void getPages() throws Exception {
+
         mockMvc.perform(get(Mappings.WELCOME))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME));
@@ -50,15 +62,20 @@ public class MainControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME));
 
-        mockMvc.perform(get(Mappings.ORDER))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("products"))
-                .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME));
-
-        mockMvc.perform(get(Mappings.BASKET))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME));
     }
 
+    @Test
+    public void testProducts() throws Exception {
 
+        // show products
+        MvcResult result = mockMvc.perform(get(Mappings.ORDER))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("products"))
+                .andExpect(model().attributeExists(Mappings.MODEL_ARGUMENT_NAME))
+                .andReturn();
+
+        ArrayList<Product> sortedProducts = (ArrayList<Product>) result.getModelAndView().getModel().get("products");
+        assertTrue(sortedProducts.size() > 0);
+    }
+    
 }
